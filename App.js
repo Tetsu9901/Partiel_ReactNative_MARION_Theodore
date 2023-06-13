cc005a3daf57ca0a2974782ba59b75318f0e69f7
Tetsu9901 <t.marion@lyon.ort.asso.fr>
2023-06-13 13:46:27 +0200

    First iteration FlatList

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+import GameList from './components/GameList';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
+      <GameList />
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
--- components/GameList.js
+++ components/GameList.js
@@ -1,4 +1,7 @@
-[
+import * as React from "react";
+import { FlatList, Text, StyleSheet, View } from "react-native";
+
+const data = [
     {
         name: "Medal of Honor",
         price: "10€",
@@ -68,3 +71,38 @@
         id: 549769,
     },
 ];
+
+const GameList = () => {
+    return (
+        <View>
+            <FlatList
+                data={data}
+                renderItem={({ item }) => <Text style={styles.text}>{item.name} | {item.price}{"\n"} #{item.catégorie} </Text> }
+                keyExtractor={(item) => item.id}
+                style={styles.liste}
+            />
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    liste: {
+        marginTop: 20,
+        backgroundColor: "#ffe9ec",
+        width: "90%",
+        maxHeight: "70%",
+        borderRadius: 10,
+    },
+    text: {
+        fontSize: 20,
+        marginLeft: 10,
+        marginRight: 10,
+        color: "black",
+        textAlign: "center",
+        marginTop: 30,
+    },
+
+
+});
+
+export default GameList;
